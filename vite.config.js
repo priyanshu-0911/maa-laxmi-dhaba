eb1ce460f98b132bb4ada60bfe97450d79b906e9
priyanshu <priyanshuramchandani41@gmail.com>
2025-07-23 23:30:22 +0530

    fix: Finalize configuration for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  // When running the local development server (`npm run dev`), command will be 'serve'
-  if (command === 'serve') {
-    return {
-      // Dev server specific config
-      plugins: [react()],
-      // No 'base' property is needed, it defaults to '/' which is correct for localhost
-    }
-  } else {
-    // When building for production (`npm run build`), command will be 'build'
-    return {
-      // Build specific config
-      plugins: [react()],
-      base: '/maa-laxmi-dhaba/', // This is ONLY used for the build, not for local dev
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
+  // No 'base' property is needed for Netlify, it handles this automatically.
 })
